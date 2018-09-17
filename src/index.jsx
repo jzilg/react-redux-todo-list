d8976a667059bae099b047295d868f453f279842
jzilg <jzg@dasburo.com>
2018-09-17 13:36:16 +0200

    add redux-dev-tools

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { applyMiddleware, createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducher from './redux/reducers'
+import rootReducer from './redux/reducers'
 import Root from './containers/Root'
 import './style/index.scss'
 
-const middleware = applyMiddleware(thunk)
-const store = createStore(rootReducher, middleware)
+const middlewareComposer = compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : store => store,
+)
+
+const store = createStore(
+    rootReducer,
+    {},
+    middlewareComposer,
+)
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'))
