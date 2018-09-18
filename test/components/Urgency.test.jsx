d0ac568105628095ea973094f4a789c50341647d
jzilg <johannes.zilg@gmail.com>
2018-09-18 22:05:29 +0200

    update tests

--- test/components/Urgency.test.jsx
+++ test/components/Urgency.test.jsx
@@ -8,46 +8,34 @@ import Urgency from '../../src/components/Urgency'
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('Urgency', () => {
-    const pastOutsideSchedule = '2018-09-01'
-    const pastInsideScheduleOneDay = '2018-09-16'
-    const pastInsideScheduleMoreDays = '2018-09-14'
+    const pastOneDayAgo = '2018-09-16'
+    const pastMoreDaysAgo = '2018-09-14'
     const today = '2018-09-17'
-    const schedule = 4
+    const schedule = 1
 
-    it('should render correcty if lastEvent was insde of schedule one day', () => {
+    it('should render "do it today" if lastEvent was one day ago and schedule is 1', () => {
         const output = shallow((
             <Urgency
                 today={today}
-                lastEvent={pastInsideScheduleOneDay}
+                lastEvent={pastOneDayAgo}
                 schedule={schedule}
             />
         ))
         expect(shallowToJson(output)).toMatchSnapshot()
     })
 
-    it('should render correcty if lastEvent was inseide of schedule more days', () => {
+    it('should render "n Days left" if was n days ago', () => {
         const output = shallow((
             <Urgency
                 today={today}
-                lastEvent={pastInsideScheduleMoreDays}
+                lastEvent={pastMoreDaysAgo}
                 schedule={schedule}
             />
         ))
         expect(shallowToJson(output)).toMatchSnapshot()
     })
 
-    it('should render correcty if lastEvent was outside of schedule', () => {
-        const output = shallow((
-            <Urgency
-                today={today}
-                lastEvent={pastOutsideSchedule}
-                schedule={schedule}
-            />
-        ))
-        expect(shallowToJson(output)).toMatchSnapshot()
-    })
-
-    it('should render correcty if lastEvent was today', () => {
+    it('should render "1 day left" if lastEvent was today', () => {
         const output = shallow((
             <Urgency
                 today={today}
@@ -58,7 +46,7 @@ describe('Urgency', () => {
         expect(shallowToJson(output)).toMatchSnapshot()
     })
 
-    it('should render correcty if last event is not set', () => {
+    it('should render "?" if last event is not set', () => {
         const output = shallow((
             <Urgency
                 today={today}
