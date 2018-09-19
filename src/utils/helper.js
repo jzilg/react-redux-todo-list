e94d4b2d0f8691bd4b45a83ca35ac6e77ba5b3c0
jzilg <johannes.zilg@gmail.com>
2018-09-19 20:50:58 +0200

    add jsdoc to helper functions

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,9 +1,17 @@
 import moment from 'moment'
 
+/**
+ * @returns {string} todays date (YYYY-MM-DD)
+ */
 export function getTodaysDate() {
     return moment().format('YYYY-MM-DD')
 }
 
+/**
+ * @param {string} startDate moment date format
+ * @param {string} endDate moment date format
+ * @returns {number} duration in days
+ */
 export function getDurationBetweenDates(startDate, endDate) {
     const startDateMoment = moment(startDate)
     const endDateMoment = moment(endDate)
