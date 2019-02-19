9097320f19ce33568656c5561a5152227bfd9933
jzilg <jzg@dasburo.com>
2019-02-19 14:35:10 +0100

    Revert "do not save new added todos automaticly"
    This reverts commit 59e1e35fa10b3d1d08f3ec6a539fb440acb40c60.

--- src/redux/actions/add-todo.actions.ts
+++ src/redux/actions/add-todo.actions.ts
@@ -1,11 +1,32 @@
-import Todo from '../../entities/todo.interface'
 import Action from '../interfaces/action.interface'
+import Todo from '../../entities/todo.interface'
+import BACKEND_URL from '../../constants/api'
 
-export const ADD_TODO = 'ADD_TODO'
+export const ADD_TODO_REQUEST = 'ADD_TODO_REQUEST'
+export const ADD_TODO_SUCCESS = 'ADD_TODO_SUCCESS'
 
-export const addTodo = (todo: Todo): Action => ({
-    type: ADD_TODO,
+export const addTodoSuccess = (todo: Todo): Action => ({
+    type: ADD_TODO_SUCCESS,
     payload: {
         todo,
     },
+    meta: {
+        showLoader: false,
+    },
 })
+
+export const addTodo = (todo: Todo): Action => {
+    const url = `${BACKEND_URL}/todos`
+    return {
+        type: ADD_TODO_REQUEST,
+        meta: {
+            api: {
+                url,
+                method: 'POST',
+                body: JSON.stringify(todo),
+                successAction: addTodoSuccess,
+            },
+            showLoader: true,
+        },
+    }
+}
--- test/redux/actions/add-todo.actions.test.js
+++ test/redux/actions/add-todo.actions.test.js
@@ -1,26 +1,61 @@
 import expect from 'expect'
 import configureMockStore from 'redux-mock-store'
-import { addTodo, ADD_TODO } from '../../../src/redux/actions/add-todo.actions'
+import BACKEND_URL from '../../../src/constants/api'
+import {
+    addTodo,
+    addTodoSuccess,
+    ADD_TODO_REQUEST,
+    ADD_TODO_SUCCESS,
+} from '../../../src/redux/actions/add-todo.actions'
 
-describe('addTodo', () => {
+describe('addEmptyTodo', () => {
     const mockStore = configureMockStore()
     const todo = {
         id: 1,
     }
 
-    it('should create the action ADD_TODO when addEmptyTodo is called', () => {
+    it('should create the action ADD_TODO_REQUEST when addEmptyTodo is called', () => {
         const store = mockStore()
+        const url = `${BACKEND_URL}/todos`
+        const method = 'POST'
+        const body = JSON.stringify(todo)
+        const successAction = addTodoSuccess
 
         const expectedActions = [
             {
-                type: ADD_TODO,
-                payload: {
-                    todo,
+                type: ADD_TODO_REQUEST,
+                meta: {
+                    api: {
+                        url,
+                        method,
+                        body,
+                        successAction,
+                    },
+                    showLoader: true,
                 },
             },
         ]
 
         store.dispatch(addTodo(todo))
+        expect(store.getActions()).toEqual(expectedActions)
+    })
+
+    it('should create the action ADD_TODO_REQUEST when addEmptyTodo is called', () => {
+        const store = mockStore()
+
+        const expectedActions = [
+            {
+                type: ADD_TODO_SUCCESS,
+                payload: {
+                    todo,
+                },
+                meta: {
+                    showLoader: false,
+                },
+            },
+        ]
+
+        store.dispatch(addTodoSuccess(todo))
         expect(store.getActions()).toEqual(expectedActions)
     })
 })
